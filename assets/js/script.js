61569b39bae1ee4c935c54d0c33422d9e02a7a39
Irene <ire.andreina.z@gmail.com>
2022-03-09 19:26:54 +0100

    add function to show players' selections

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,8 @@ const choices = [
 const userDisplay = document.querySelector('.user-selection');
 const computerDisplay = document.querySelector('.computer-selection');
 const messageDisplay = document.querySelector('.message');
+const gameDisplay = document.querySelector('.user-options-container');
+const battleDisplay = document.querySelector('.battle-results');
 
 //From Math Project, Code Institute and adapted for this project 
 //Wait for the DOM to finish loading before running the game
@@ -40,16 +42,16 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let button of buttons) {
 
         button.addEventListener('click', function() {
-            
+            //Will identify the clicked button
             const optionSelected = this.getAttribute('data-option');
-            const userOption = (choices.find(choice => choice.name === optionSelected)).name;
+            //this is the user's choice linked with the choices array
+            const userOption = choices.find(choice => choice.name === optionSelected);
 
-            console.log(`1 ${userOption}`);
+            console.log(`1 ${userOption.name}`);
             gameStart(userOption);
         })
     }
 })
-
 
 //function for having a random choice for the computer
 function computerRandomChoice() {
@@ -59,8 +61,26 @@ function computerRandomChoice() {
 }
 
 //function for starting the game, it iniciates after user clicks on a button
-function gameStart(userChoice) {
-    const computerChoice = computerRandomChoice();
-    const choice = computerChoice.name;
+function gameStart(userOption) {
+    const computerSelection = computerRandomChoice();
+    const choice = computerSelection.name;
+    const userSelection = userOption.name;
+    display(userOption, computerSelection);
+    
+   
     console.log(`2 ${choice}`);
+}
+
+function display (userSelection, computerSelection) {
+    gameDisplay.style.display = 'none';
+    battleDisplay.style.display = 'block';
+
+    const imgUser = document.createElement('img');
+    const imgComputer = document.createElement('img');
+
+    imgUser.src = userSelection.image;
+    imgComputer.src = computerSelection.image;
+
+    userDisplay.appendChild(imgUser);
+    computerDisplay.appendChild(imgComputer);
 }
